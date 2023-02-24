bdc59830efa6a563bbc03e5286b070859e1e8d07
Karthik Uppuluri <koopuluri@gmail.com>
2023-02-24 23:22:11 +0530

    polish

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -6,7 +6,7 @@ function getHighlightedText(text: string, highlight: string) {
     // Split on highlight term and include term into parts, ignore case
     const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
     return <span> { parts.map((part, i) => 
-        <span key={i} style={part.toLowerCase() === highlight.toLowerCase() ? { color: 'white' } : {} }>
+        <span key={i} style={part.toLowerCase() === highlight.toLowerCase() ? { color: '#95E994' } : {} }>
             { part }
         </span>)
     } </span>;
@@ -38,11 +38,13 @@ export default function SearchResult(props: {
         
         return (
             <div className={styles.textLineContainer}>
-                <a className={styles.annotation} href={destination} target="_blank">{annotation}</a>
+                <a className={styles.annotation + " " + styles.positionedLeft} href={destination} target="_blank">{annotation}</a>
                 {highlighted}
             </div>
         )
     })
+
+    let title = getHighlightedText(props.title, props.searchTerm)
 
     return (
         <div className={styles.container}>
@@ -51,9 +53,8 @@ export default function SearchResult(props: {
                 <span className={styles.duration}>{props.duration}</span>
             </div>
             <div className={styles.textContainer}>
-                <a href={props.destination} target="_blank"><h1 className={styles.title}>{props.title}</h1></a>
-                <span className={styles.when}>{props.when}</span>
-                <span className={styles.channel}>{props.channel}</span>
+                <a href={props.destination + "&t=" + props.text[0].seconds} target="_blank"><h1 className={styles.title}>{title}</h1></a>
+                <span className={styles.when}>{props.channel} | {props.when}</span>
             </div>
             <div className={styles.text}>
                 {texts}
